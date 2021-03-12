cc4f44cb054d7e663eb70be65046436a3ce186c8
josaphamorim07 <josaphamorim05@gmail.com>
2021-03-12 00:38:51 -0400

    creation pages and components

---
+++ my-pokedex/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+    <nav className="container-header">
+        
+       <label>imagem</label>
+        
+    </nav>
+    )
+}
+
+export default Header
