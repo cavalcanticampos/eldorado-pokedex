7af53e37d846e321341eb84cd420a84cb18d6de9
josaphamorim07 <josaphamorim05@gmail.com>
2021-03-12 00:49:25 -0400

    new pages

--- my-pokedex/src/components/header/Header.js
+++ my-pokedex/src/components/header/Header.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 function Header() {
     return (
-    <nav className="container-header">
-        
-       <label>imagem</label>
-        
-    </nav>
+        <nav className="container-header">
+             <h1>header</h1>
+        </nav>
     )
 }
 
