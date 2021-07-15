600fca6745004ac48c44c41efd48c9a01ed73342
josaphamorim07 <josaphamorim05@gmail.com>
2021-07-15 19:35:20 -0400

    pokemosn listings api

---
+++ backend/src/main/java/br/com/pokeapi/controller/PokemonController.java
@@ -0,0 +1,48 @@
+package br.com.pokeapi.controller;
+import br.com.pokeapi.model.Pokemon;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/pokemons")
+public class PokemonController {
+
+    List<Pokemon>  pokeList = new ArrayList<>();
+    Pokemon poke  = new Pokemon();
+
+    @GetMapping("/list")
+    public List<Pokemon> findAllPoke() {
+
+        pokeList.add( new  Pokemon(1,"Bulbassaur", "#70A83B",49,49,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"));
+        pokeList.add( new  Pokemon(2,"ivysaur","#70A83B",62,63,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"));
+        pokeList.add( new  Pokemon(3,"venusaur","#70A83B",82,83,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"));
+        pokeList.add( new  Pokemon(4,"charmander","  #F76545",52,43,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"));
+        pokeList.add( new  Pokemon(5,"charmeleon","  #F76545",64,58,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg"));
+        pokeList.add( new  Pokemon(6,"charizard","  #F76545",84,78,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"));
+        pokeList.add( new  Pokemon(7,"squirtle","A2CFF0",48,65,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"));
+        pokeList.add( new  Pokemon(8,"wartortle","A2CFF0",63,80,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg"));
+        pokeList.add( new  Pokemon(9,"blastoise","A2CFF0",83,100,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg"));
+
+        return  pokeList;
+    }
+
+    @PostMapping("/creating")
+    public List<Pokemon>  creating(@RequestBody Pokemon poke){
+        pokeList.add(poke);
+        return pokeList;
+    }
+
+
+
+    @GetMapping("/list/{id}") public Pokemon findAllById(@PathVariable("id") Integer id){
+
+        Optional<Pokemon>  pokeFind = pokeList.stream().filter(pokeID -> pokeID.getId() == id).findFirst();
+        if (pokeFind.isPresent()) {
+            return pokeFind.get();
+        }
+        return null;
+    }
+}
---
+++ backend/src/main/java/br/com/pokeapi/model/Pokemon.java
@@ -0,0 +1,20 @@
+package br.com.pokeapi.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Pokemon {
+
+     private Integer id;
+     private String name;
+     private String backgroundColor;
+     private int attack;
+     private int defense;
+     private String type;
+     private String url;
+}
