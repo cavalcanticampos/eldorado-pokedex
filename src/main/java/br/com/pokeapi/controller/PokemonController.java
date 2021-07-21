1e39fe6f5c6c1a5a66a005d417b0e6d52b3bebda
josaphamorim07 <josaphamorim05@gmail.com>
2021-07-21 08:32:44 -0400

    update pagination

--- src/main/java/br/com/pokeapi/controller/PokemonController.java
+++ src/main/java/br/com/pokeapi/controller/PokemonController.java
@@ -1,10 +1,8 @@
 package br.com.pokeapi.controller;
 import br.com.pokeapi.model.Pokemon;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+
+import java.util.*;
 
 
 @RestController
@@ -21,20 +19,25 @@ public class PokemonController {
     public  List<Pokemon>  findAllPoke( int page){
 
 
+        List<Pokemon>  pokeList = new ArrayList<>(Arrays.asList(
 
-        pokeList.add( new  Pokemon(1,"Bulbassaur", "#70A83B",49,49,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"));
-        pokeList.add( new  Pokemon(2,"ivysaur","#70A83B",62,63,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"));
-        pokeList.add( new  Pokemon(3,"venusaur","#70A83B",82,83,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"));
-        pokeList.add( new  Pokemon(4,"charmander","  #F76545",52,43,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"));
-        pokeList.add( new  Pokemon(5,"charmeleon","  #F76545",64,58,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg"));
-        pokeList.add( new  Pokemon(6,"charizard","  #F76545",84,78,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"));
-        pokeList.add( new  Pokemon(7,"squirtle","A2CFF0",48,65,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"));
-        pokeList.add( new  Pokemon(8,"wartortle","A2CFF0",63,80,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg"));
-        pokeList.add( new  Pokemon(9,"blastoise","A2CFF0",83,100,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg"));
-        pokeList.add( new  Pokemon(10,"pikachu","A2CFF0",83,100,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg"));
+                new  Pokemon(1,"Bulbassaur", "#70A83B",49,49,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"),
+        new  Pokemon(2,"ivysaur","#70A83B",62,63,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"),
+        new  Pokemon(3,"venusaur","#70A83B",82,83,"grass","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"),
+        new  Pokemon(4,"charmander","  #F76545",52,43,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"),
+        new  Pokemon(5,"charmeleon","  #F76545",64,58,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg"),
+        new  Pokemon(6,"charizard","  #F76545",84,78,"fire","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"),
+        new  Pokemon(7,"squirtle","A2CFF0",48,65,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"),
+        new  Pokemon(8,"wartortle","A2CFF0",63,80,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg"),
+        new  Pokemon(9,"blastoise","A2CFF0",83,100,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg"),
+        new  Pokemon(10,"pikachu","A2CFF0",83,100,"water","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg")
 
 
-        int pageSize=2;
+        ));
+
+
+
+        int pageSize=9;
         int offeset = (page-1) * pageSize;
         if(pokeList == null || pokeList.size() <= offeset){
             return Collections.emptyList();
