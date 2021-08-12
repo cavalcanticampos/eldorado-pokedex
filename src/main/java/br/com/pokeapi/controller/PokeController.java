bb3071f6aed1d87a9e2defb1e663142b76ce41d4
josaphamorim07 <josaphamorim05@gmail.com>
2021-08-12 10:16:55 -0400

    update 7

---
+++ src/main/java/br/com/pokeapi/controller/PokeController.java
@@ -0,0 +1,32 @@
+package br.com.pokeapi.controller;
+
+
+import br.com.pokeapi.model.ListPokemons;
+import br.com.pokeapi.model.PokemonResults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/pokemons"})
+
+public class PokeController {
+
+    @GetMapping
+    public List findAll(){
+
+        RestTemplate res = new RestTemplate();
+
+        ListPokemons response = res.getForObject("https://pokeapi.co/api/v2/pokemon?limit=200&offset=0", ListPokemons.class);
+        List<PokemonResults> results = response.getResults();
+
+
+        return results;
+
+
+    }
+
+}
