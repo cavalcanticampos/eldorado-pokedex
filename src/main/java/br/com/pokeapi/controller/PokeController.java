70d1eebd03a923e701adb3a4f7a7386d5a99e3cc
josaphamorim07 <josaphamorim05@gmail.com>
2021-08-12 11:12:35 -0400

    update 9

--- src/main/java/br/com/pokeapi/controller/PokeController.java
+++ src/main/java/br/com/pokeapi/controller/PokeController.java
@@ -2,12 +2,14 @@ package br.com.pokeapi.controller;
 
 
 import br.com.pokeapi.model.ListPokemons;
+import br.com.pokeapi.model.Pokemon;
 import br.com.pokeapi.model.PokemonResults;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,13 +20,20 @@ public class PokeController {
     @GetMapping
     public List findAll(){
 
+        List<Pokemon> array = new ArrayList<>();
+        int i;
+
         RestTemplate res = new RestTemplate();
 
-        ListPokemons response = res.getForObject("https://pokeapi.co/api/v2/pokemon?limit=200&offset=0", ListPokemons.class);
+        ListPokemons response = res.getForObject("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0", ListPokemons.class);
         List<PokemonResults> results = response.getResults();
 
+        for(i=0; i< results.size(); i++){
+            Pokemon pokemon = res.getForObject(results.get(i).getUrl(),Pokemon.class);
+            array.add(pokemon);
+            }
 
-        return results;
+        return array;
 
 
     }
--- src/main/java/br/com/pokeapi/model/PokemonResults.java
+++ src/main/java/br/com/pokeapi/model/PokemonResults.java
@@ -1,11 +1,12 @@
 package br.com.pokeapi.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-
+@AllArgsConstructor
 @NoArgsConstructor
 @Data
 
