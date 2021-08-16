2859a515f8abbd134f55c2949b2be84e8650d27e
josaphamorim07 <josaphamorim05@gmail.com>
2021-08-15 21:35:23 -0400

    6

--- src/main/java/br/com/pokeapi/controller/PokeController.java
+++ src/main/java/br/com/pokeapi/controller/PokeController.java
@@ -1,6 +1,10 @@
 package br.com.pokeapi.controller;
 import br.com.pokeapi.model.Pokemon;
 import br.com.pokeapi.model.PokemonResults;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,25 +15,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pokemons")
-
 public class PokeController {
 
     @GetMapping
     public List findAll(){
 
         List<Pokemon> array = new ArrayList<>();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
         int i;
 
-        RestTemplate res = new RestTemplate();
-
+        RestTemplate res = new RestTemplate(requestFactory);
         PokemonResults response = res.getForObject("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0", PokemonResults.class);
-
         System.out.println(response);
-
         List poke = response.getResults();
-
         return poke ;
-
 
     }
 
