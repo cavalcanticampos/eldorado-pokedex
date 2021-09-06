9934a510161e2685f55395c12ee66ea6ac5820ea
Roberto Campos-ROBCAMP <robcamp@motorola.com>
2021-09-06 10:01:41 -0400

    new version 0.2.0

--- frontend/src/Breakstyles.js
+++
@@ -1,6 +0,0 @@
-  export const breakpoints = {
-  mobile: '425px',
-  tablet: '768px',
-  laptop:  '1383px',
-  desktop: '1920px',
-}
---
+++ precache-manifest.eecce619d065c7749d82fee6f7490dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82bb0f06334d7e91e7d3",
+    "url": "./static/js/main.614f5fed.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8f2c8873ed7b3d318e9b",
+    "url": "./static/js/2.e0f988b2.chunk.js"
+  },
+  {
+    "revision": "39a69c1c27f7ef4369dfd792dfa92616",
+    "url": "./static/media/Banner.39a69c1c.png"
+  },
+  {
+    "revision": "0b39ad4dbaf533f31e1c6279fa504510",
+    "url": "./static/media/BannerComplete.0b39ad4d.svg"
+  },
+  {
+    "revision": "595f2377e6c4c551a39a17cc7e2c17a8",
+    "url": "./index.html"
+  }
+];
